d5a0d8c71870a491cdca1ecd8dc8d1a4b9029886
NightFeather <ss900328@gmail.com>
2016-09-18 22:22:25 +0800

    Add Dummy Data
    and loader

--- routes/room.js
+++ routes/room.js
@@ -1,5 +1,19 @@
 function room(io){
 	var crypto = require("crypto");
+	var fs = require("fs");
+
+	var quest_list ={}
+
+	fs.readFile('./public/data/quiz.json', 'utf8', function (err, data) {
+    if (!err) {
+      quest_list = JSON.parse(data);
+    }
+    else {
+      throw err;
+    }
+  });
+
+
 	io.on('connect',function(client){
 		client.on('createRoom', function(data){
 			room_id = crypto.randomBytes(20).toString("hex");
